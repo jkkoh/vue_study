a44d22144dcd156f601c2c3d52522165a5242eac
jkkoh <jeongkyu7@gmail.com>
2021-11-19 15:33:43 +0900

    aaa

---
+++ src/data/list.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        name : 'HTML1',
+        tag : 'html'
+    },
+    {
+        name : 'HTML2',
+        tag : 'html'
+    },
+    {
+        name : 'HTML3',
+        tag : 'html'
+    },
+    {
+        name : 'CSS1',
+        tag : 'css'
+    },
+    {
+        name : 'CSS2',
+        tag : 'css'
+    },
+    {
+        name : 'CSS3',
+        tag : 'css'
+    },
+    {
+        name : 'JAVASCRIPT1',
+        tag : 'javascript'
+    },
+    {
+        name : 'JAVASCRIPT2',
+        tag : 'javascript'
+    },
+    {
+        name : 'JAVASCRIPT3',
+        tag : 'javascript'
+    },
+    {
+        name : 'Vue1',
+        tag : 'vue'
+    },
+    {
+        name : 'Vue2',
+        tag : 'vue'
+    },
+    {
+        name : 'Vue3',
+        tag : 'vue'
+    },
+    {
+        name : 'SCSS1',
+        tag : 'scss',
+    },
+    {
+        name : 'SCSS2',
+        tag : 'scss',
+    },
+    {
+        name : 'SCSS3',
+        tag : 'scss',
+    },
+]
